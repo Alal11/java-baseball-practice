301ff01c85dbe6ef86b4d7f945920fcf7e143326
Alal11 <spalqj56@naver.com>
2024-02-22 22:06:24 +0900

    view 내용 구축

--- src/main/java/baseball/view/GamePrint.java
+++ src/main/java/baseball/view/GamePrint.java
@@ -2,26 +2,29 @@ package baseball.view;
 
 public class GamePrint {
 
-    public void start() {
-        System.out.println("숫자 야구 게임을 시작합니다.");
+    private static final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+    private static final String GAME_INPUT_MESSAGE = "숫자를 입력해주세요 : ";
+    public static final String GAME_END_MESSAGE = "개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    private static final String GAME_RESTART_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    public static final String STRIKE_PRINT = "스트라이크";
+    public static final String BALL_PRINT = "볼";
+    public static final String NOTHING_PRINT = "낫싱";
+    private static final String ERROR_MESSAGE = "잘못된 입력값 입니다.";
+
+
+    public static void printStart() {
+        System.out.println(GAME_START_MESSAGE);
     }
 
-    public void end() {
-        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    public static void printInputNumber() {
+        System.out.println(GAME_INPUT_MESSAGE);
     }
 
-    public void resultPrint(int[] sbCountArr) {
-        if (sbCountArr[1] != 0) {
-            System.out.print(sbCountArr[1] + "볼");
-        }
-        if (sbCountArr[0] != 0) {
-            System.out.println(sbCountArr[0] + "스트라이크");
-            if (sbCountArr[0] == 3) {
-                System.out.println(sbCountArr[0] + "개의 숫자를 모두 맞히셨습니다! 게임 종료");
-            }
-        }
-        if (sbCountArr[0] == 0 && sbCountArr[1] == 0) {
-            System.out.println("낫싱");
-        }
+    public static void printInputError() {
+        System.out.println(ERROR_MESSAGE);
+    }
+
+    public static void printReEnd() {
+        System.out.println(GAME_RESTART_MESSAGE);
     }
 }
